1f771a52b681c78f03bb8d744fa319a1305ffb40
Michael Rynkiewicz <michaelrynkiewicz3@gmail.com>
2017-08-16 20:39:59 +0200

    Added music layout

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
@@ -9,8 +11,13 @@ export class MusicComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  files = [
+    "This house is not for sale",
+    "Can't go home",
+    "More than you know"
+  ];
 
+  ngOnInit() {
   }
 
 }
